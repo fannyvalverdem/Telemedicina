eb4d5af67bd74e1acb08665f747d6164881a81f3
ivinces <ivinces@espol.edu.ec>
2019-08-21 12:06:04 -0500

    url

--- WEB/medicina/urls.py
+++ WEB/medicina/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path,include,re_path
+from django.urls import path,include
 from django.conf.urls import url
 from . import views
 
@@ -11,10 +11,10 @@ urlpatterns = [
 	url(r'^medico_emergencia/$', views.selec_med_emergencia, name='selec_med_emergencia'),
 	url(r'^confirmacion_emergencia/$', views.confirmacion_emergencia, name='confirmacion_emergencia'),
 	url(r'^conteo_citas/$', views.conteo_citas, name='conteo_citas'),
-	url(r'^tarifas/$', views.tarifas, name='tarifas'),
+	url(r'^tarifas/', views.tarifas, name='tarifas'),
 	url(r'^ing_paquete/$', views.ingresar_paquete, name='ing-paquete'),
 	url(r'^registro/', views.registro, name='registro'),
 	url(r'^paciente/$', views.index_paciente, name='index_paciente'),
 	url(r'^medico/$', views.index_medico, name='index_medico'),
-	url(r'^administrador/$', views.index_admin, name='index_admin'),
+	url(r'^administrador/', views.index_admin, name='index_admin'),
 ]
