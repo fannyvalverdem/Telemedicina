6ebe02bf6e8fb0e3f5ffed89e5b534405b3ec320
ivinces <ivinces@espol.edu.ec>
2019-09-09 21:29:30 -0500

    pp

--- WEB/static/js/Tabla_medico.js
+++ WEB/static/js/Tabla_medico.js
@@ -79,3 +79,36 @@ $('#data_table_paquete').DataTable({
         }},
     ],
 });
+
+$('#data_table_citas').DataTable({
+    "destroy": true,
+    "ajax": 
+        {
+        "method": "GET",
+        "url": "/api/consulta/",
+        "dataSrc": "",
+        "error": function(xhr, status, error) {
+            console.log("readyState: " + xhr.readyState);
+            console.log("responseText: "+ xhr.responseText);
+            console.log("status: " + xhr.status);
+            console.log("text status: " + status);
+            console.log("error: " + error);
+        },
+    
+    },
+    
+    "columns": [
+        { data: "consulta_id.paciente_id.user_id.persona_id.nombre"},
+        { data: "consulta_id.paciente_id.user_id.persona_id.apellido"},
+        { data: "fecha_reser"},
+        { data: "fecha_prog"},
+        { data: "precio"}
+    ],
+    columnDefs: [
+        { width: 100, targets: 0},
+        { width: 150, targets: 1},
+        { width: 150, targets: 2},
+        { width: 150, targets: 3},
+        { width: 150, targets: 4},
+    ],
+});
