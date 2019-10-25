6308521011f90ec2718d53d9f14ebd21f564f3a0
Fanny <fmvalver@espol.edu.ec>
2019-10-25 17:35:16 -0500

    [#168514310] alguito mas

--- WEB/medicina/views.py
+++ WEB/medicina/views.py
@@ -545,7 +545,7 @@ def escribir_receta(request):
 	if request.method == 'POST':
 		print("POST")
 		
-	form= Receta(request.POST)
+	form= RecetaForm(request.POST)
 	if form.is_valid():
 		name =form.cleaned_data['name']
 		apellido =form.cleaned_data['apellido']
@@ -575,5 +575,5 @@ def escribir_receta(request):
 
 		return redirect('index_medico')
 	else:
-		form=Receta()
+		form=RecetaForm()
 	return render(request, 'escribir_receta.html', {'form':form})
