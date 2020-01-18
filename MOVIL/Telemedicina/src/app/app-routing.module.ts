5d3025fab7b111a76ad3d2d0617e7c195998bb36
pedrojms <pmendoza@espol.edu.ec>
2020-01-17 21:19:46 -0500

    side menu

--- MOVIL/Telemedicina/src/app/app-routing.module.ts
+++ MOVIL/Telemedicina/src/app/app-routing.module.ts
@@ -12,7 +12,8 @@ const routes: Routes = [
   { path: 'principal', loadChildren: './principal/principal.module#PrincipalPageModule' },
   { path: 'agendar-cita-esp', loadChildren: './agendar-cita-esp/agendar-cita-esp.module#AgendarCitaEspPageModule' },
   { path: 'agendar-cita-med', loadChildren: './agendar-cita-med/agendar-cita-med.module#AgendarCitaMedPageModule' },
-  { path: 'agendar-cita-conf', loadChildren: './agendar-cita-conf/agendar-cita-conf.module#AgendarCitaConfPageModule' },  { path: 'perfil', loadChildren: './perfil/perfil.module#PerfilPageModule' },
+  { path: 'agendar-cita-conf', loadChildren: './agendar-cita-conf/agendar-cita-conf.module#AgendarCitaConfPageModule' },
+  { path: 'perfil', loadChildren: './perfil/perfil.module#PerfilPageModule' },
   { path: 'videollamada', loadChildren: './videollamada/videollamada.module#VideollamadaPageModule' },
   { path: 'pago', loadChildren: './pago/pago.module#PagoPageModule' },
   { path: 'receta', loadChildren: './receta/receta.module#RecetaPageModule' },
@@ -21,6 +22,7 @@ const routes: Routes = [
   { path: 'receta-espec', loadChildren: './receta-espec/receta-espec.module#RecetaEspecPageModule' },
   { path: 'paquetes', loadChildren: './paquetes/paquetes.module#PaquetesPageModule' },
   { path: 'contacto', loadChildren: './contacto/contacto.module#ContactoPageModule' }
+  
 
 ];
 @NgModule({
--- MOVIL/Telemedicina/src/app/app.component.ts
+++ MOVIL/Telemedicina/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  pages=[
+    {
+      title:'Contactenos',
+      url:'/contacto'
+    }
+  ];
+
+  selectedPath = '';
+
+  
+
+
+
+
   constructor(
+    private router: Router,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    this.router.events.subscribe((event:RouterEvent)=> {
+      this.selectedPath = event.url;
+    });
     this.initializeApp();
   }
 
